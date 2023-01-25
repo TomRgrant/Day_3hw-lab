82793d165420d6cbe7749a5c24f470d5fce8997e
Tom Grant <grntthomasronnie@gmail.com>
2023-01-25 18:24:27 +0000

    Added homework/lab work

---
+++ src/python_functions_practice.py
@@ -0,0 +1,127 @@
+def return_10():
+    return 10
+
+def add(num_1,num_2):
+    return num_1 + num_2
+
+def subtract(num_1, num_2):
+    return num_1 - num_2
+
+def multiply(num_1, num_2):
+    return num_1 * num_2
+
+def divide(num_1, num_2):
+    return num_1 / num_2
+
+def length_of_string(string):
+    return len(string)
+
+def join_string(str_1, str_2):
+    return str_1 + str_2
+
+def add_string_as_number(str_1, str_2):
+    return int(str_1) + int(str_2)
+
+def number_to_full_month_name(num):
+    if num == 1:
+        return "January"
+    elif num == 3:
+        return "March"
+    elif num == 9:
+        return "September"
+
+def number_to_short_month_name(num):
+    if num == 1:
+        return "Jan"
+    elif num == 4:
+        return "Apr"
+    elif num == 10:
+        return "Oct"
+
+def volume_of_cube(length):
+    return length**3
+
+def reverse_string(str):
+    return str [::-1]
+
+def fahrenheit_to_celsius(fahrenheit):
+    celcius = round(((fahrenheit - 32) * 0.5556))
+    return celcius
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
